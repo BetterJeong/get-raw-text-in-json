f9b3302ac83588a3261032f685eb39a5be4e17d7
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-09-16 20:43:40 +0900

    텍스트 파일 개별 저장

--- json_parsing.py
+++ json_parsing.py
@@ -2,7 +2,7 @@ import json
 import os
 
 
-def extract_raw_text_from_json(json_file_path, output_text_file_path):
+def extract_raw_text_from_json(json_file_path):
     # JSON 파일 읽기
     with open(json_file_path, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
@@ -13,20 +13,27 @@ def extract_raw_text_from_json(json_file_path, output_text_file_path):
     return raw_texts
 
 
-def process_all_json_files_in_directory(input_directory, output_text_file_path):
-    # 디렉터리 내의 모든 JSON 파일 경로 가져오기
+def process_all_json_files_in_directory(input_directory, output_directory):
+    # 모든 JSON 파일 경로 가져오기
     json_files = [f for f in os.listdir(input_directory) if f.endswith('.json')]
 
-    # 각 JSON 파일에서 raw_text 추출 및 텍스트 파일로 저장
-    with open(output_text_file_path, 'w', encoding='utf-8') as text_file:
-        for json_file in json_files:
-            json_file_path = os.path.join(input_directory, json_file)
-            raw_texts = extract_raw_text_from_json(json_file_path, output_text_file_path)
+    for json_file in json_files:
+        json_file_path = os.path.join(input_directory, json_file)
+        raw_texts = extract_raw_text_from_json(json_file_path)
+
+        # JSON 파일과 동일한 이름의 텍스트 파일 생성 및 저장
+        base_filename = os.path.splitext(json_file)[0]
+        output_text_file_path = os.path.join(output_directory, base_filename + '.txt')
+
+        with open(output_text_file_path, 'w', encoding='utf-8') as text_file:
             for raw_text in raw_texts:
                 text_file.write(raw_text + '\n')
 
 
-input_directory = '.'
-output_text_file_path = 'output.txt'
+input_directory = 'json_files'  # JSON 파일 경로
+output_directory = 'text_files'  # 텍스트 파일 경로
 
-process_all_json_files_in_directory(input_directory, output_text_file_path)
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+process_all_json_files_in_directory(input_directory, output_directory)
